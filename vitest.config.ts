6f9b92401485a2f03349960d1459dcace2dcc4bb
Luis Eduardo <eduardo.devop@gmail.com>
2026-03-11 23:20:06 +0000

    fix: update test file inclusion in TypeScript configuration for Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ const twoMinutes = 2 * 60 * 1000; // 2 minutes * 60 seconds * 1000 milliseconds
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.test.ts", "tests/**/*.test.ts"],
+    include: ["src/**/*.test.ts", "tests/**/*.test.ts", "e2e/**/*.test.ts"],
     testTimeout: twoMinutes,
     hookTimeout: twoMinutes,
   },
